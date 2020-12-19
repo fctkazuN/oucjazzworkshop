773563438a71ce83c93654f03401a39cf0859c2e
fctkazuN <fct.kazuma.nagao@gmail.com>
2020-12-19 16:16:01 +0900

    フッタ修正

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,15 +1,14 @@
-import React, { useEffect, useState } from "react";
-import clsx from "clsx";
+import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import { useLocation } from "react-router";
 import { Follow } from "react-twitter-widgets";
 
 const useStyles = makeStyles(() => ({
   vertical: {
     display: "flex",
     flexDirection: "column",
+    marginTop: "auto",
   },
 }));
 
@@ -18,17 +17,6 @@ const useStyles = makeStyles(() => ({
  */
 const Footer: React.FC = () => {
   const classes = useStyles();
-  const [isSticky, setIsSticky] = useState(false);
-  const location = useLocation();
-
-  useEffect(() => {
-    const content = document.getElementById("content") as HTMLElement;
-    if (content.scrollHeight < window.innerHeight * 0.9) {
-      setIsSticky(true);
-    } else {
-      setIsSticky(false);
-    }
-  }, [location]);
 
   return (
     <Toolbar
